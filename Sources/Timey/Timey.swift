9f1491b71cf6cf900a9d94a23a4b378521055800
Jesper Westervig Nielsen <jesper@app-it.dk>
2020-08-06 14:01:58 +0200

    Added Float extension

--- Sources/Timey/Timey.swift
+++ Sources/Timey/Timey.swift
@@ -14,3 +14,18 @@ extension IntegerLiteralType {
         return self * 60 * 60
     }
 }
+
+extension FloatLiteralType {
+    
+    public var seconds: Self {
+        return self
+    }
+    
+    public var minutes: Double {
+        return self * 60
+    }
+
+    public var hours: Double {
+        return self * 60 * 60
+    }
+}
